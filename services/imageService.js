0a2d3491294989a3ec32af3bd2e98f4d81a3cd07
PD670 <prantadas85463@gmail.com>
2025-03-20 01:37:28 +0600

    changes

--- services/imageService.js
+++ services/imageService.js
@@ -5,4 +5,19 @@ export const  getUserImageSrc = imagePath => {
     else {
         return require('../assets/images/defaultUser.png');
     }
+
+}
+
+export const uploadFile = async (folderName, fileUri, isImage=true) => {
+    try {
+        let fileName = getFilePath(folderName, isImage);
+    } catch (error) {
+        console.log('file upload error: ', error);
+        return {success : false, msg: 'could not upload media'};
+        
+    }
+}
+export const getFilePath =(folderName, isImage) => {
+    return `/${folderName}/${(new Date()).getTime()}${isImage? '.png': '.mp4'}`;
+
 }
