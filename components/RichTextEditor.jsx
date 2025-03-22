e509f2b46cb501904dd3bd48b10b3b9a252319d5
PD670 <prantadas85463@gmail.com>
2025-03-22 23:37:59 +0600

    changes

---
+++ components/RichTextEditor.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RichTextEditor = () => {
+    editorRef,
+    onChange
+  return (
+    <View style={{minHeight: 285}}>
+      <Text>RichTextEditor</Text>
+    </View>
+  )
+}
+
+export default RichTextEditor
+
+const styles = StyleSheet.create({})
