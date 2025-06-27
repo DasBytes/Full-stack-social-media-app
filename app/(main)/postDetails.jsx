0e45f9c7a852167723b5694285707b4b0d05f4c9
PD670 <prantadas85463@gmail.com>
2025-06-27 12:36:36 +0600

    changes

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -1,10 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
+import { fetchPostDetails } from "../../services/postService";
+import { hp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
 
 const PostDetails = () => {
   const { postId } = useLocalSearchParams();
   console.log("got post Id: ", postId);
+
+  const [post, setPost] = useState[null];
+
+  useState(()=> {
+    getPostDetails();
+  },[
+
+  ]);
+
+  const getPostDetails = async() => {
+    // fetch post details heres
+    let res = await fetchPostDetails(postId);
+    if(res.success) setPost(res.data);
+
+    
+  }
 
   return (
     <View>
@@ -15,4 +34,27 @@ const PostDetails = () => {
 
 export default PostDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+  sendIcon: {
+    borderColor: theme.colors.primary,
+    borderRadius: theme.radius.lg,
+    borderCurve: 'continuous',
+    height: hp(5.8),
+    width: hp(5.8),
+
+  },
+
+  center : {
+   flex: 1,
+   alignItems:'center',
+   justifyContent: 'center',
+  },
+  notFound : {
+    fontSize: hp(2.5),
+    color: theme.colors.text,
+    fontWeight: theme.fonts.medium,
+
+  },
+  
+});
